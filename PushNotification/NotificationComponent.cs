8f325b837cc8e72e063630f321cff9e1b525a29e
Nahid Hasan <nahid675@diu.edu.bd>
2017-08-12 12:28:31 +0600

    Add project files.

---
+++ PushNotification/NotificationComponent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using Microsoft.AspNet.SignalR;
+
+namespace PushNotification
+{
+    public class NotificationComponent
+    {
+        //Here we will add a function for register notification (will add sql dependency)
+        public void RegisterNotification(DateTime currentTime)
+        {
+            string conStr = ConfigurationManager.ConnectionStrings["sqlConString"].ConnectionString;
+            string sqlCommand = @"SELECT [ID],[Title],[Message] from [dbo].[ProductList] where [DateTime] > @DateTime";
+            //you can notice here I have added table name like this [dbo].[Contacts] with [dbo], its mendatory when you use Sql Dependency
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                SqlCommand cmd = new SqlCommand(sqlCommand, con);
+                cmd.Parameters.AddWithValue("@DateTime", currentTime);
+                if (con.State != System.Data.ConnectionState.Open)
+                {
+                    con.Open();
+                }
+                cmd.Notification = null;
+                SqlDependency sqlDep = new SqlDependency(cmd);
+                sqlDep.OnChange += sqlDep_OnChange;
+                //we must have to execute the command here
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    // nothing need to add here now
+                }
+            }
+        }
+
+        void sqlDep_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Type == SqlNotificationType.Change)
+            {
+                SqlDependency sqlDep = sender as SqlDependency;
+                sqlDep.OnChange -= sqlDep_OnChange;
+
+                //from here we will send notification message to client
+                var notificationHub = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+                notificationHub.Clients.All.notify("added");
+
+                //re-register notification
+                RegisterNotification(DateTime.Now);
+
+            }
+        }
+
+        public List<ProductList> GetContacts(DateTime afterDate)
+        {
+            using (MyPushNotificationEntities dc = new MyPushNotificationEntities())
+            {
+                return dc.ProductLists.Where(a => a.DateTime > afterDate).OrderByDescending(a => a.DateTime).ToList();
+            }
+        }
+    }
+}
